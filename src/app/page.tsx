a301a55e2317445d275fdfd4cdc370c6f6d437c1
Leo Sperandio <leomartins64@yahoo.com.br>
2025-06-11 22:04:28 -0300

    fix: remove import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React from 'react';
-import Background from '@/components/common/Background';
 import LandingPage from '@/components/LandingPage';
 
 export default function Home() {
